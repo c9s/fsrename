1789e65289776c7fb25810b7880f0f3da1b7feec
c9s <yoanlin93@gmail.com>
2015-05-31 12:59:17 +0800

    Update option name

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import "strings"
 
 var matchPatternPtr = flag.String("match", ".", "regular expression without slash '/'")
 var replacementPtr = flag.String("replace", "", "replacement")
-var fileOnlyPtr = flag.Bool("fileonly", false, "file only")
-var dirOnlyPtr = flag.Bool("dironly", false, "directory only")
+var fileOnlyPtr = flag.Bool("fileOnly", false, "file only")
+var dirOnlyPtr = flag.Bool("dirOnly", false, "directory only")
 var forExtPtr = flag.String("forExt", "", "extension name")
 var dryPtr = flag.Bool("dry", false, "dry run only")
 
