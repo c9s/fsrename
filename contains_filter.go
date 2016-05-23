495e33f68aa185d399709660569dd797829aed08
c9s <yoanlin93@gmail.com>
2016-05-23 15:48:01 +0800

    Add contains filter

---
+++ contains_filter.go
@@ -0,0 +1,30 @@
+package fsrename
+
+import "strings"
+
+type StrContainsFilter struct {
+	*BaseWorker
+	Search string
+}
+
+func NewStrContainsFilter(search string) *StrContainsFilter {
+	return &StrContainsFilter{NewBaseWorker(), search}
+}
+
+func (w *StrContainsFilter) Run() {
+	for {
+		select {
+		case <-w.stop:
+			return
+		case entry := <-w.input:
+			// end of data
+			if entry == nil {
+				w.emitEnd()
+				return
+			}
+			if strings.Contains(entry.base, w.Search) {
+				w.output <- entry
+			}
+		}
+	}
+}
